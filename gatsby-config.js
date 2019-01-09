e67bf240d86fba3ff9087ee10f3ba12dffb1b598
Jeff Reiner <theundeadghost@gmail.com>
2019-01-09 23:42:28 +0100

    :art: cleaning up packages and some markup

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -26,9 +17,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    },
+    `gatsby-plugin-netlify`
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" />
     <h1>hello world</h1>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
