944c967424f48678547e160584d48012aa83b33f
Jeff Reiner <theundeadghost@gmail.com>
2019-01-10 16:15:57 +0100

    :sparkles: adding seo meta and lambda path proxy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,36 @@
-module.exports = {
+import proxy from "http-proxy-middleware";
+
+export default {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Centrifuge - The Operating System For Global Commerce`,
+    description: `Centrifuge is an open, decentralized operating system to connect the global financial supply chain. It allows any business to transact on a global network while maintaining ownership of their data, including their validated company details, their reputation, business relationships, and subsequent transactions.`,
+    author: `@centrifuge`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `centrifuge-website`,
+        short_name: `centrifuge`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fcba59`,
+        theme_color: `#fcba59`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-netlify`
-  ]
+  ],
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      proxy({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": ""
+        }
+      })
+    );
+  }
 };
