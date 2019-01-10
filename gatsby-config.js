cc7ff6b4242e37d46b6338bea8135b6393fe39a5
Jeff Reiner <theundeadghost@gmail.com>
2019-01-10 18:00:03 +0100

    :sparkles: adding in gatsby-source-apiserver for lambda

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,79 @@
-import proxy from "http-proxy-middleware";
+const proxy = require("http-proxy-middleware");
 
-export default {
+const netlifyLambdaEndpoint = `${process.env.URL}/.netlify/functions`;
+
+const breezyServerOptions = {
+  // Type prefix of entities from server
+  typePrefix: "lambda__",
+
+  // The url, this should be the endpoint you are attempting to pull data from
+  url:
+    process.env.NODE_ENV == "production"
+      ? `${netlifyLambdaEndpoint}/getBreezyJobListings`
+      : `http://localhost:9000/getBreezyJobListings`,
+
+  method: "GET",
+
+  headers: {
+    "Content-Type": "application/json"
+  },
+
+  // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+  // using this name. i.e. posts.json
+  name: `breezy`,
+
+  // Define schemaType to normalize blank values
+  schemaType: {
+    id: 1,
+    position: "String",
+    link: "String",
+    location: "String",
+    offering: "String"
+  },
+
+  // Optionally include some output when building
+  // Default is false
+  verboseOutput: true // For debugging purposes
+};
+
+const gitcoinServerOptions = {
+  // Type prefix of entities from server
+  typePrefix: "lambda__",
+
+  // The url, this should be the endpoint you are attempting to pull data from
+  url:
+    process.env.NODE_ENV == "production"
+      ? `${netlifyLambdaEndpoint}/getGitcoinBounties`
+      : `http://localhost:9000/getGitcoinBounties`,
+
+  method: "GET",
+
+  headers: {
+    "Content-Type": "application/json"
+  },
+
+  // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+  // using this name. i.e. posts.json
+  name: `gitcoin`,
+
+  // Define schemaType to normalize blank values
+  // schemaType: {
+  //   id: 1,
+  //   position: "String",
+  //   link: "String",
+  //   location: "String",
+  //   offering: "String"
+  // },
+
+  // Optionally include some output when building
+  // Default is false
+  verboseOutput: true // For debugging purposes
+};
+
+module.exports = {
   siteMetadata: {
-    title: `Centrifuge - The Operating System For Global Commerce`,
+    title: `Centrifuge`,
+    longTitle: `Centrifuge - The Operating System For Global Commerce`,
     description: `Centrifuge is an open, decentralized operating system to connect the global financial supply chain. It allows any business to transact on a global network while maintaining ownership of their data, including their validated company details, their reputation, business relationships, and subsequent transactions.`,
     author: `@centrifuge`
   },
@@ -19,6 +90,14 @@ export default {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: "gatsby-source-apiserver",
+      options: breezyServerOptions
+    },
+    {
+      resolve: "gatsby-source-apiserver",
+      options: gitcoinServerOptions
     },
     `gatsby-plugin-netlify`
   ],
