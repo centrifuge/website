1a5416ecd4ddf280b02269d022951dd3756ae2ca
Jeff Reiner <theundeadghost@gmail.com>
2019-01-18 15:24:25 +0100

    :sparkles: adding animations to technology page

--- src/pages/technology/index.js
+++ src/pages/technology/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Lottie from "lottie-react-web";
 
 import Layout from "../../components/Layout";
 import SEO from "../../components/SEO";
-
 import Container from "../../components/Container";
 import Grid from "../../components/Grid";
-import Column from "../../components/Column";
+import Column, { Spacer } from "../../components/Column";
 
-import { ParseJSXToReact } from "../../helpers";
+import { ParseJSXToReact, lastInArray } from "../../helpers";
+
+import block1Animation from "../../lottie/Technology.json";
+import bigAnimation from "../../lottie/Technology_BIG.json";
 
 const TechnologyPage = ({ data }) => {
   const page = data.allContentfulPageTechnology.edges[0].node;
@@ -17,19 +20,38 @@ const TechnologyPage = ({ data }) => {
     <Layout>
       <SEO {...page.seo} />
       <Container>
+        {/* Block 1 */}
         <Grid>
-          <Column span={{ small: 12, medium: 6, large: 4 }}>
+          <Column span={{ small: 12, medium: 12, large: 7 }}>
             <ParseJSXToReact block={page.block1} />
           </Column>
+          <Spacer />
+          <Column span={{ small: 12, medium: 12, large: 4 }}>
+            <Lottie options={{ animationData: block1Animation }} />
+          </Column>
         </Grid>
-        <Grid align="start">
-          {page.block2.map(block => (
-            <Column span={{ small: 12, medium: 12, large: 4 }}>
-              <ParseJSXToReact block={block.content} />
-            </Column>
+
+        {/* Block 2 */}
+        <Grid justify="start" align="start">
+          {page.block2.map((block, index) => (
+            <>
+              <Column span={{ small: 12, medium: 12, large: 3 }}>
+                <ParseJSXToReact block={block.content} />
+              </Column>
+              {!lastInArray(page.block2, index) && <Spacer />}
+            </>
           ))}
         </Grid>
+
+        {/* Animation Block */}
         <Grid>
+          <Column>
+            <Lottie options={{ animationData: bigAnimation }} />
+          </Column>
+        </Grid>
+
+        {/* Block 3 */}
+        <Grid justify="center">
           <Column span={{ small: 12, medium: 12, large: 12 }}>
             <ParseJSXToReact block={page.block3} />
           </Column>
