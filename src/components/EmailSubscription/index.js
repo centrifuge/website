53336ad37fb044623300ac80aa60bb67586c31ac
Jeff Reiner <theundeadghost@gmail.com>
2019-01-21 15:42:00 +0100

    :recycle: Remove grommet bs from this

--- src/components/EmailSubscription/index.js
+++ src/components/EmailSubscription/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Box, TextInput, Button } from "grommet";
 import addToMailchimp from "gatsby-plugin-mailchimp";
+import styled from "styled-components";
 
 import Container from "../Container";
+
+const FormWrapper = styled.div`
+  display: grid;
+  grid-gap: 24px;
+
+  @media only screen and (min-width: 424px) {
+    grid-template-columns: 1fr max-content;
+  }
+`;
 
 class SubscriptionForm extends React.Component {
   constructor(props) {
@@ -31,22 +41,21 @@ class SubscriptionForm extends React.Component {
     if (!this.state.submitted) {
       return (
         <form onSubmit={this.handleSubmit}>
-          <Box justify="center" align="center" direction="row" gap="medium">
-            <Box flex="grow">
-              <TextInput
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                placeholder="l@centrifuge.io"
-              />
-            </Box>
+          <FormWrapper>
+            <TextInput
+              required
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="l@centrifuge.io"
+            />
             <Button
               type="submit"
               disabled={this.state.submitDisabled}
               alignSelf="center"
               label="Join the list"
             />
-          </Box>
+          </FormWrapper>
         </form>
       );
     }
