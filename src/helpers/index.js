a11415aa98323017de436288d65dcee17c496314
Jeff Reiner <theundeadghost@gmail.com>
2019-01-25 18:06:25 +0100

    :art: exporting richTextRenderer from helpers
    g s

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import ParseJSXToReact from "./parseJsxToReact";
+import RichTextRenderer from "./richTextRenderer";
 
 const getRawHtml = block => block.childContentfulRichText.html;
 
@@ -7,4 +8,10 @@ const createDangerousMarkup = block => ({ __html: getRawHtml(block) });
 const lastInArray = (block, index) =>
   block.length === index + 1 ? true : false;
 
-export { lastInArray, getRawHtml, createDangerousMarkup, ParseJSXToReact };
+export {
+  lastInArray,
+  getRawHtml,
+  createDangerousMarkup,
+  ParseJSXToReact,
+  RichTextRenderer
+};
