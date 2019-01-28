16a41cbb027d3a899c95c2679c8a4b99bbae259c
Jeff Reiner <theundeadghost@gmail.com>
2019-01-28 18:44:13 +0100

    :truck: fixing navigation folder spelling issue :P

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import Theme from "../Theme";
 import Footer from "../Footer";
-import Navigation from "../Navgation";
+import Navigation from "../Navigation";
 import EmailSubscription from "../EmailSubscription";
 
 const Layout = ({ children }) => (
