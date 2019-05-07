52e43f3875749efa91495bd1a6f36e24c54baafe
ark.gorecki <aaron@bejamas.io>
2019-05-07 19:41:29 +0200

    fix#118 prefetch API calls

--- lambda/getGitcoinOpenBounties.js
+++ lambda/getGitcoinOpenBounties.js
@@ -1,20 +1,16 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
 exports.handler = async (event, context) =>
   fetch(
-    `https://gitcoin.co/api/v0.1/bounties/?network=mainnet&order_by=-_val_usd_db&org=centrifuge&limit=100`
+    `https://gitcoin.co/api/v0.1/bounties/?network=mainnet&order_by=-_val_usd_db&org=centrifuge&limit=100`,
   )
     .then(res => res.json())
-    .then(res =>
-      res
-        .filter(bounty => bounty.status != "cancelled")
-        .filter(bounty => bounty.status != "done")
-    )
+    .then(res => res.filter(bounty => !['cancelled', 'done'].includes(bounty.status)))
     .then(json => ({
       statusCode: 200,
-      body: JSON.stringify(json)
+      body: JSON.stringify(json),
     }))
     .catch(error => ({
       statusCode: 422,
-      body: JSON.stringify(error)
-    }));
+      body: JSON.stringify(error),
+    }))
---
+++ utils/prefetch_urls.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch')
+
+function prefetchURLs() {
+  console.log('prefetching urls to decrease lamdba functions execution time')
+  return fetch(
+    `https://gitcoin.co/api/v0.1/bounties/?network=mainnet&order_by=-_val_usd_db&org=centrifuge&limit=100`,
+  ).then(() => fetch(
+      `https://gitcoin.co/api/v0.1/bounties/?network=mainnet&idx_status=done&order_by=-_val_usd_db&org=centrifuge&limit=100`,
+    )
+    .then(() => Promise.resolve())
+    .catch(err => console.log(err))
+  )
+}
+
+prefetchURLs()
