69767058eeb56a9032b20f4a7134b95ffd8e1c1f
Sounak Pradhan <sounak.98@gmail.com>
2021-05-02 17:03:59 +0530

    feat: udpate about page

---
+++ src/components/Team/index.js
@@ -0,0 +1,130 @@
+import React, { useEffect } from "react";
+import styled, { css } from "styled-components";
+import { Box, Text, Image, Anchor, Button } from "grommet";
+import Grid from "../Grid";
+import Column from "../Column";
+import teamMembers from "./teamMembers";
+
+import twitter from "../../images/team-social/twitter.svg";
+import instagram from "../../images/team-social/instagram.svg";
+import linkedin from "../../images/team-social/linkedin.svg";
+import favourite from "../../images/team-social/favourite.svg";
+
+const MemberImageContainer = styled(Box)`
+  padding-bottom: 100%;
+  position: relative;
+  border-radius: 20px;
+
+  ${props =>
+    css`
+      background-image: url("${props.src}");
+      background-size: cover;
+      background-position: center center;
+    `}
+
+  :hover div {
+    display: block;
+  }
+`;
+
+const MemberImageLayover = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #fcba59;
+  border-radius: 20px;
+  display: none;
+`;
+
+const Member = ({ member }) => {
+  const getSocialIcon = socialLink => {
+    if (socialLink.includes("linkedin")) return <Image src={linkedin} />;
+    else if (socialLink.includes("twitter")) return <Image src={twitter} />;
+    else if (socialLink.includes("instagram")) return <Image src={instagram} />;
+  };
+
+  return (
+    <Box gap="medium">
+      <MemberImageContainer src={member.image}>
+        {/*<Image*/}
+        {/*  src={member.image}*/}
+        {/*  style={{*/}
+        {/*    borderRadius: "20px"*/}
+        {/*  }}*/}
+        {/*/>*/}
+        <MemberImageLayover>
+          <Box
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50% , -50%)",
+              textAlign: "center"
+            }}
+          >
+            <Text textAlign="center" color="white" weight={500}>
+              {member.description}
+            </Text>
+          </Box>
+        </MemberImageLayover>
+      </MemberImageContainer>
+
+      <Box direction="row" justify="between">
+        <Text truncate>{member.name}</Text>
+        <Box direction="row" gap="small">
+          {member.social && (
+            <Anchor href={member.social} target="_blank">
+              {getSocialIcon(member.social)}
+            </Anchor>
+          )}
+          <Anchor href={member.link} target="_blank">
+            <Image src={favourite} />
+          </Anchor>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const Team = () => {
+  return (
+    <Box>
+      <Box align="center" gap="small">
+        <Text
+          size="xxlarge"
+          weight={500}
+          textAlign="center"
+          style={{ lineHeight: "50px" }}
+        >
+          Who is building Centrifuge?
+        </Text>
+        <Text size="large" color="dark-4">
+          Contributors
+        </Text>
+      </Box>
+      <Grid mt="large" mb="large" gap="36">
+        {teamMembers
+          .sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+          })
+          .map(member => (
+            <Column
+              span={{ small: 6, large: 3, medium: 4 }}
+              justifySelf="stretch"
+              margin={{ bottom: "medium" }}
+            >
+              <Member member={member} />
+            </Column>
+          ))}
+      </Grid>
+      {/* button should be of the color: #F44E72 */}
+      <Button primary alignSelf="center" label="work with us" />
+    </Box>
+  );
+};
+
+export default Team;
---
+++ src/components/Team/teamMembers.js
@@ -0,0 +1,176 @@
+import alina from "../../images/team/alina.jpeg";
+import lea from "../../images/team/lea.jpg";
+import miguel from "../../images/team/miguel.png";
+import aubry from "../../images/team/aubry.png";
+import helena from "../../images/team/helena.jpeg";
+import lisa from "../../images/team/lisa.jpg";
+import mike from "../../images/team/mike.jpg";
+import cassidy from "../../images/team/cassidy.jpg";
+import jason from "../../images/team/jason.jpg";
+import lucas from "../../images/team/lucas.png";
+import ved from "../../images/team/ved.jpeg";
+import dennis from "../../images/team/dennis.jpg";
+import jeroen from "../../images/team/jeroen.png";
+import manuel from "../../images/team/manuel.png";
+import devin from "../../images/team/devin.jpg";
+import jp from "../../images/team/jp.jpg";
+import marc from "../../images/team/marc.jpg";
+import dylan from "../../images/team/dylan.jpg";
+import kate from "../../images/team/kate.jpeg";
+import martin from "../../images/team/martin.png";
+
+const teamMembers = [
+  {
+    name: "Dylan Dedi",
+    description: "Communicating Centrifuge inside and out",
+    link: "https://fees.wtf/",
+    social: "https://www.linkedin.com/in/dylandedi/",
+    image: dylan
+  },
+  {
+    name: "Helena Flack",
+    description: "Communicating to the outside world",
+    link: "https://decentralala.com/",
+    social: "https://twitter.com/helsfoftroy",
+    image: helena
+  },
+  {
+    name: "Manuel Polzhofer",
+    description: "Building software together with an awesome team",
+    link: "https://ourworldindata.org/",
+    social: "https://twitter.com/m_polzhofer",
+    image: manuel
+  },
+  {
+    name: "Lea Schmitt",
+    description: "DeFi-Centrifuge match making",
+    link: "https://www.givewell.org/giving",
+    social: "https://twitter.com/_schmitted",
+    image: lea
+  },
+  {
+    name: "Dennis Wellmann",
+    description: "Decentralizing financial products",
+    link: "https://www.espncricinfo.com/",
+    social: "https://twitter.com/financeundtech",
+    image: dennis
+  },
+  {
+    name: "Kate Beecroft",
+    description: "Ecosystem strategizing and community weaving",
+    link: "https://nywolf.org/meet-our-wolves/webcams/",
+    social: "https://twitter.com/kbeecroft",
+    image: kate
+  },
+  {
+    name: "Lisa Plesiutschnig",
+    description:
+      "Ensuring a pleasant working environment and an appreciative workplace for the team",
+    link: "https://www.itsnicethat.com",
+    social: "https://www.instagram.com/liezens/",
+    image: lisa
+  },
+  {
+    name: "Vedhavyas Singareddi",
+    description: "Collaborate on building Centrifuge together",
+    link: "https://www.producthunt.com/",
+    social: "https://twitter.com/Svedhavyas",
+    image: ved
+  },
+  {
+    name: "Miguel Hervas",
+    description:
+      "Building infrastructures that enable the creation of decentralized products",
+    link: "https://news.ycombinator.com/",
+    social: "https://twitter.com/mikiquantum",
+    image: miguel
+  },
+  {
+    name: "Jeroen Offerijns",
+    description: "Building a great product for our users",
+    link: "https://hckrnews.com/",
+    social: "https://twitter.com/Offerijns",
+    image: jeroen
+  },
+  {
+    name: "Aubry Kessel",
+    description: "Being like a Swiss army knife",
+    link: "https://www.barrons.com/",
+    social: "https://www.instagram.com/aubs.23/",
+    image: aubry
+  },
+  {
+    name: "Jason Jones",
+    description: "Building a business around the technology",
+    link: "https://www.benlcollins.com/blog/",
+    social: "https://www.linkedin.com/in/jasonjoneslinkedin/",
+    image: jason
+  },
+  {
+    name: "Cassidy Daly",
+    description:
+      "Creating tools that allow people to have open access to the world’s financial system",
+    link: "https://duckduckgo.com/",
+    social: "https://www.twitter.com/cassidydaly",
+    image: cassidy
+  },
+  {
+    name: "Alina Sinelnikova",
+    description: "Building DeFi products together with the best team",
+    link: "https://www.wolframalpha.com/",
+    social: "https://www.linkedin.com/in/alina-sinelnikova-5474a782/",
+    image: alina
+  },
+  {
+    name: "Martin Quensel",
+    description: "Figure out how to replace CeFi with DeFi products",
+    link:
+      "https://www.visualcapitalist.com/all-of-the-worlds-money-and-markets-in-one-visualization-2020/",
+    social: "https://twitter.com/mr_optimax",
+    image: martin
+  },
+  {
+    name: "Lucas Vogelsang",
+    description: "Develop & communicate the vision for Centrifuge",
+    link: "about:blank",
+    social: "https://twitter.com/lucasvo",
+    image: lucas
+  },
+  {
+    name: "Mike Ruzic",
+    description: "Bringing assets and investors to Centrifuge",
+    link: "https://www.globalcyclingnetwork.com/",
+    social: "https://www.linkedin.com/in/michael-ruzic-gauthier/",
+    image: mike
+  },
+  {
+    name: "Marc A. Brown",
+    description: "Bringing Centrifuge to the moon (why not Mars)",
+    link: "https://homotopytypetheory.org/",
+    image: marc
+  },
+  {
+    name: "Devin Black",
+    description: "Accounts and Beyond",
+    link: "https://zombo.com/",
+    social: "https://www.linkedin.com/in/devin-b/",
+    image: devin
+  },
+  {
+    name: "JP Angelle",
+    description:
+      "Building accessible and intuitive user interfaces in the DeFi space",
+    link: "https://atomic.blue/degenscore",
+    social: "https://www.linkedin.com/in/jeanpaulangelle/",
+    image: jp
+  }
+  // {
+  //   name: "Frederik Schulz",
+  //   description: "Building infrastructure for DeFi",
+  //   link: "https://en.wikipedia.org/",
+  //   social: "https://www.linkedin.com/in/frederik-lukas-schulz/",
+  //   image:
+  // }
+];
+
+export default teamMembers;
