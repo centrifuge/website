b907fde26db7364fdf8d5e9f1ee85e956dc59958
JP Angelle <jp@angelle.me>
2021-06-30 12:29:51 +0100

    ignore polkadot library

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,25 @@
-const path = require("path");
+const path = require('path');
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ loaders, actions }) => {
   actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: path.resolve(
+            __dirname,
+            'node_modules',
+            '@polkadot/extension-dapp',
+          ),
+          use: loaders.null(),
+        },
+      ],
+    },
     resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        images: path.resolve(__dirname, "src/images")
-      }
-    }
+        components: path.resolve(__dirname, 'src/components'),
+        images: path.resolve(__dirname, 'src/images'),
+      },
+    },
   });
 };
