f78c024d29b27fbc5e17a59f0b2db78438d03a8e
Alessio Scalici <alessio@centrifuge.io>
2021-10-20 15:26:33 +0200

    chore: migrate to gatsby v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,28 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const siteUrl = process.env.URL || "http://localhost:8000";
 
-const lambdaServerDefaults = filename => ({
+const lambdaServerDefaults = (filename) => ({
   typePrefix: "lambda__",
   data: {},
   method: "GET",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   },
   url:
     process.env.NODE_ENV == "production"
       ? `${process.env.URL}/.netlify/functions/${filename}`
-      : `http://localhost:9000/${filename}`
+      : `http://localhost:9000/${filename}`,
 });
 
 const mediumPostsServerOptions = {
   name: `mediumPosts`,
   ...lambdaServerDefaults("getMediumPosts"),
-  verboseOutput: true
+  verboseOutput: true,
 };
 
 module.exports = {
@@ -35,15 +35,15 @@ module.exports = {
     lambdaUrl:
       process.env.NODE_ENV == "production"
         ? `${process.env.URL}/.netlify/functions/`
-        : `http://localhost:9000/`
+        : `http://localhost:9000/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-robots-txt`,
       options: {
-        sitemap: `${siteUrl}/sitemap.xml`
-      }
+        sitemap: `${siteUrl}/sitemap.xml`,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -54,51 +54,51 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#2762ff`,
         display: `minimal-ui`,
-        icon: `src/images/centrifuge-logo.png` // This path is relative to the root of the site.
-      }
+        icon: `src/images/centrifuge-logo.png`, // This path is relative to the root of the site.
+      },
     },
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
-        endpoint: `https://centrifuge.us17.list-manage.com/subscribe/post?u=27084e1d9e6f92398b5c7ce91&amp;id=e00b1ece80`
-      }
+        endpoint: `https://centrifuge.us17.list-manage.com/subscribe/post?u=27084e1d9e6f92398b5c7ce91&amp;id=e00b1ece80`,
+      },
     },
     {
       resolve: "gatsby-source-apiserver",
-      options: mediumPostsServerOptions
+      options: mediumPostsServerOptions,
     },
     {
       resolve: `gatsby-transformer-yaml-plus`,
       options: {
         enableRemark: true,
-        markdownPreface: "md//"
-      }
+        markdownPreface: "md//",
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./content/`
-      }
+        path: `./content/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/images/`
-      }
+        path: `./src/images/`,
+      },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "fonts",
-        path: `${__dirname}/static/fonts/`
-      }
+        path: `${__dirname}/static/fonts/`,
+      },
     },
     {
       resolve: `gatsby-source-lever`,
       options: {
         site: `centrifuge`,
-        verboseOutput: false
-      }
+        verboseOutput: false,
+      },
     },
     {
       resolve: `gatsby-plugin-mdx`,
@@ -107,43 +107,45 @@ module.exports = {
           {
             resolve: `gatsby-remark-copy-linked-files`,
             options: {
-              ignoreFileExtensions: []
-            }
-          }
+              ignoreFileExtensions: [],
+            },
+          },
         ],
         extensions: [`.md`],
         defaultLayouts: {
-          default: require.resolve(`./src/components/MDXLayout/index.js`)
-        }
-      }
+          default: require.resolve(`./src/components/MDXLayout/index.js`),
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId:
-          process.env.URL === "https://centrifuge.io" ? `UA-100764518-2` : `none`,
+          process.env.URL === "https://centrifuge.io"
+            ? `UA-100764518-2`
+            : `none`,
         head: false,
         anonymize: true,
         respectDNT: true,
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "centrifuge.io"
-      }
+        cookieDomain: "centrifuge.io",
+      },
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify-cache`
+    `gatsby-plugin-netlify-cache`,
   ],
-  developMiddleware: app => {
+  developMiddleware: (app) => {
     app.use(
       "/.netlify/functions/",
-      proxy({
+      createProxyMiddleware({
         target: "http://localhost:9000",
         pathRewrite: {
-          "/.netlify/functions/": ""
-        }
+          "/.netlify/functions/": "",
+        },
       })
     );
-  }
+  },
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === 'build-html') {
+  if (stage === "build-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
             test: path.resolve(
               __dirname,
-              'node_modules/@polkadot/extension-dapp',
+              "node_modules/@polkadot/extension-dapp"
             ),
             use: loaders.null(),
           },
@@ -18,10 +18,12 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   }
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
       alias: {
-        components: path.resolve(__dirname, 'src/components'),
-        images: path.resolve(__dirname, 'src/images'),
+        components: path.resolve(__dirname, "src/components"),
+        images: path.resolve(__dirname, "src/images"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
       },
     },
   });
