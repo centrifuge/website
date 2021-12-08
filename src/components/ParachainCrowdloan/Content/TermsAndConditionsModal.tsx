40b2a27104a570bc520a3f29e67ddd753a02aae5
Alessio Scalici <alessio@centrifuge.io>
2021-12-08 10:20:43 +0100

    chore: update terms and conditions

--- src/components/ParachainCrowdloan/Content/TermsAndConditionsModal.tsx
+++ src/components/ParachainCrowdloan/Content/TermsAndConditionsModal.tsx
@@ -29,15 +29,14 @@ export const TermsAndConditionsModal: React.FC<TermsAndConditionsModalProps> = (
           By submitting the form your DOT will be locked on Polkadot for the
           Centrifuge parachain crowdloan. This means that your DOT will be
           locked for the duration of the parachain slot if Centrifuge wins the
-          auction (48 weeks), or until the auction ends if Centrifuge does not
-          win the auction. The initial transferrable amount of DOT reward is
-          25%. The remaining vests over the lease period of 48 weeks. Proxy or
-          multi-signature accounts are not able to receive rewards. Use of this
-          page and the above staking function are at your own risk. Further,
-          Centrifuge makes no warranties as to the outcome of the Centrifuge
-          crowdloan. To the fullest extent allowed by applicable law, in no
-          event shall Centrifuge or its affiliates, be liable to you or any
-          third party for any damages of any kind.
+          auction (96 weeks), or until the auction ends if Centrifuge does not
+          win the auction. The CFG reward vests over the lease period of 96
+          weeks. Proxy or multi-signature accounts are not able to receive
+          rewards. Use of this page and the above staking function are at your
+          own risk. Further, Centrifuge makes no warranties as to the outcome of
+          the Centrifuge crowdloan. To the fullest extent allowed by applicable
+          law, in no event shall Centrifuge or its affiliates, be liable to you
+          or any third party for any damages of any kind.
         </Text>
       </Box>
     </Layer>
