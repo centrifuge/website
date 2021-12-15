ca8559fe33f3ea0a304cc4aeea81bea62cfc3fd2
Alessio Scalici <alessio@centrifuge.io>
2021-12-15 15:51:53 +0100

    chore: change maximum cap to soft cap

--- src/components/ParachainCrowdloan/Hero/AuctionStatusProgress.tsx
+++ src/components/ParachainCrowdloan/Hero/AuctionStatusProgress.tsx
@@ -79,7 +79,7 @@ export const AuctionStatusProgress: React.FC<AuctionStatusProgressProps> = ({
         </FadeInDiv>
         <FadeInDiv show={maxCap != null}>
           <TextValue>{maxCap != null && formatDOT(maxCap, 0, true)}</TextValue>
-          <TextLabel>DOT maximum cap</TextLabel>
+          <TextLabel>DOT soft cap</TextLabel>
         </FadeInDiv>
       </ProgressRow>
       <AuctionProgressBar maxCap={maxCap || 0} stackedAmount={stackedAmount} />
