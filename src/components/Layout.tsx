c5a92a73bd8d92c49b71fe6116c7d3e5ee588722
Hornebom <you@hornebom.com>
2022-10-04 13:03:16 +0200

    styling header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,34 @@
 import * as React from 'react'
 import centrifugeLight from '@centrifuge/fabric/dist/theme/centrifugeLight'
-import { GlobalStyle } from '@centrifuge/fabric'
-import { FontStyle } from './RootStyle'
+import { FabricProvider, GlobalStyle as FabricGlobalStyle } from '@centrifuge/fabric'
+import { GlobalStyle } from './GlobalStyle'
 import { ThemeProvider } from 'styled-components'
-import { Header } from './Header'
+import { Header } from './header'
 import { Footer } from './Footer'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
+console.log('centrifugeLight', centrifugeLight)
+
+// export function Layout({ children }: LayoutProps) {
+//   return (
+//     <FabricProvider theme={centrifugeLight}>
+//       <FabricGlobalStyle />
+//       <GlobalStyle />
+//       <Header />
+//       <main>{children}</main>
+//       <Footer />
+//     </FabricProvider>
+//   )
+// }
+
 export function Layout({ children }: LayoutProps) {
   return (
     <ThemeProvider theme={centrifugeLight}>
+      <FabricGlobalStyle theme={centrifugeLight} />
       <GlobalStyle />
-      <FontStyle />
       <Header />
       <main>{children}</main>
       <Footer />
---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { Container, Shelf } from '@centrifuge/fabric'
+import { Logo } from '../Logo'
+import { MobileMenu } from '../MobileMenu'
+import { DesktopMenu } from '../DesktopMenu'
+import { Root, LogoLink } from './styles'
+
+export function Header() {
+  return (
+    <Root as="header" px={2} height={[50, 65, 65]}>
+      <Container as={Shelf} alignItems="center" height="100%">
+        <LogoLink to="/" title="Home page">
+          <Logo />
+        </LogoLink>
+
+        {/* <MobileMenu /> */}
+        {/* <DesktopMenu /> */}
+      </Container>
+    </Root>
+  )
+}
