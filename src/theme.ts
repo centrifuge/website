1e09a9b4415b83a7d0e0a73e0d4ce73e478cfe64
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2022-10-05 13:42:00 +0200

    update theme

---
+++ src/theme.ts
@@ -0,0 +1,102 @@
+import centrifugeLight from '@centrifuge/fabric/dist/theme/centrifugeLight'
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  ...centrifugeLight,
+  sizes: {
+    ...centrifugeLight.sizes,
+    container: '1430px',
+    containerHeader: '1600px',
+    containerNarrow: '1140px',
+  },
+  typography: {
+    ...centrifugeLight.typography,
+    heading1: {
+      fontSize: [20, 80],
+      lineHeight: 1.167,
+      fontWeight: [600, 400],
+      color: 'textPrimary',
+    },
+    heading2: {
+      fontSize: [18, 72],
+      lineHeight: 1.2,
+      fontWeight: [600, 500],
+      color: 'textPrimary',
+    },
+    heading3: {
+      fontSize: [16, 56],
+      lineHeight: 1.375,
+      fontWeight: 500,
+      color: 'textPrimary',
+    },
+    heading4: {
+      fontSize: 32,
+      lineHeight: 1.375,
+      fontWeight: 500,
+      color: 'textPrimary',
+    },
+    heading5: {
+      fontSize: 28,
+      lineHeight: 1.4,
+      fontWeight: 600,
+      color: 'textPrimary',
+    },
+    heading6: {
+      fontSize: 24,
+      lineHeight: 1.2,
+      fontWeight: 400,
+      color: 'textSecondary',
+    },
+    tag: {
+      fontSize: 16,
+      lineHeight: 1.2,
+      fontWeight: 500,
+      color: 'textDisabled',
+    },
+    emphasized: {
+      fontWeight: 600,
+    },
+    interactive1: {
+      fontSize: 14,
+      lineHeight: 1.375,
+      fontWeight: 500,
+      color: 'textPrimary',
+    },
+    interactive2: {
+      fontSize: 12,
+      lineHeight: 1.375,
+      fontWeight: 500,
+      color: 'textPrimary',
+    },
+    body1: {
+      fontSize: [16, 20],
+      lineHeight: 1.3,
+      fontWeight: 400,
+      color: 'textPrimary',
+    },
+    body2: {
+      fontSize: 16,
+      lineHeight: 1.25,
+      fontWeight: 400,
+      color: 'textPrimary',
+    },
+    body3: {
+      fontSize: 14,
+      lineHeight: 1.35,
+      fontWeight: 400,
+      color: 'textPrimary',
+    },
+    body4: {
+      fontSize: 10,
+      lineHeight: 1.25,
+      fontWeight: 400,
+      color: 'textSecondary',
+    },
+    label1: {
+      fontSize: [14, 16],
+      lineHeight: 1.375,
+      fontWeight: 500,
+      color: 'textSecondary',
+    },
+  },
+}
