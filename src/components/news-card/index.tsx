ea60f0f968943d49f5ddb8591c5ef0aab70861c2
Hornebom <you@hornebom.com>
2022-10-24 11:01:11 +0200

    Loading state NewsCard
    - updates fabric dependency
    - adds loading state for Text and Media

--- src/components/news-card/index.tsx
+++ src/components/news-card/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Shelf, Grid, Text, Stack } from '@centrifuge/fabric'
+import { Box, Shelf, Grid, Text, Stack, TextWithPlaceholder } from '@centrifuge/fabric'
 import React from 'react'
-import { useTheme } from 'styled-components'
+import styled, { useTheme, keyframes } from 'styled-components'
 import { Anchor } from './styles'
 
 export type NewsCardProps = MediaProps &
@@ -27,13 +27,17 @@ export function NewsCard({
 
   return featured ? (
     <Box>
-      <Label>{label}</Label>
+      <Label isLoading={isLoading}>{label}</Label>
       <Grid gridTemplateColumns={['1fr', '1fr', 'repeat(2, minmax(0, 1fr))']} gap={[2, 2, 4]} mt={[1, 1, 2]}>
-        <Media image={image} order={[1, 1, 2]} />
+        <Media image={image} order={[1, 1, 2]} isLoading={isLoading} />
 
         <Stack gap={2} order={[2, 2, 1]}>
-          <Title featured={featured}>{title}</Title>
-          <Body featured={featured}>{body}</Body>
+          <Title featured={featured} isLoading={isLoading}>
+            {title}
+          </Title>
+          <Body featured={featured} isLoading={isLoading}>
+            {body}
+          </Body>
           <ReadMore href={href} boxed={false} />
         </Stack>
       </Grid>
@@ -53,13 +57,17 @@ export function NewsCard({
         boxShadow: boxed ? shadows.cardInteractive : 'none',
       }}
     >
-      <Label>{label}</Label>
+      <Label isLoading={isLoading}>{label}</Label>
 
-      <Media image={image} />
+      <Media image={image} isLoading={isLoading} />
 
       <Shelf mt={2} gap={1} flexDirection="column" alignItems="start" flexGrow={2}>
-        <Title featured={featured}>{title}</Title>
-        <Body featured={featured}>{body}</Body>
+        <Title featured={featured} isLoading={isLoading}>
+          {title}
+        </Title>
+        <Body featured={featured} isLoading={isLoading}>
+          {body}
+        </Body>
 
         <Box mt="auto" pt={boxed ? 0 : 1}>
           <ReadMore href={href} boxed={boxed} />
@@ -69,27 +77,43 @@ export function NewsCard({
   )
 }
 
-function Label({ children }: { children: React.ReactNode }) {
+function Label({ children, isLoading = false }: { children: React.ReactNode; isLoading?: boolean }) {
   return (
-    <Text as="span" variant="tag">
+    <TextWithPlaceholder as="span" variant="tag" isLoading={isLoading}>
       {children}
-    </Text>
+    </TextWithPlaceholder>
   )
 }
 
-function Title({ featured = false, children }: { featured: boolean; children: React.ReactNode }) {
+function Title({
+  featured = false,
+  children,
+  isLoading = false,
+}: {
+  featured: boolean
+  children: React.ReactNode
+  isLoading?: boolean
+}) {
   return (
-    <Text as="h2" variant={featured ? 'heading5' : 'heading4'}>
+    <TextWithPlaceholder as="h2" variant={featured ? 'heading5' : 'heading4'} isLoading={isLoading}>
       {children}
-    </Text>
+    </TextWithPlaceholder>
   )
 }
 
-function Body({ featured = false, children }: { featured: boolean; children: React.ReactNode }) {
+function Body({
+  featured = false,
+  children,
+  isLoading = false,
+}: {
+  featured: boolean
+  children: React.ReactNode
+  isLoading?: boolean
+}) {
   return (
-    <Text as="p" variant={featured ? 'body1' : 'body2'}>
+    <TextWithPlaceholder as={isLoading ? 'div' : 'p'} variant={featured ? 'body1' : 'body2'} isLoading={isLoading}>
       {children}
-    </Text>
+    </TextWithPlaceholder>
   )
 }
 
@@ -119,11 +143,35 @@ function ReadMore({ href, boxed = false }: ReadMoreProps) {
 
 type MediaProps = { image: string }
 
-function Media({ image, order = 0 }: MediaProps & { order?: number | number[] }) {
+const load = keyframes`
+  from {
+	  background-position-x: 0;
+  }
+  to {
+	  background-position-x: -200%;
+  }
+`
+
+const MediaPlaceholder = styled(Box)`
+  --color1: ${({ theme }) => theme.colors.borderPrimary};
+  --color2: ${({ theme }) => theme.colors.borderSecondary};
+
+  background: linear-gradient(90deg, var(--color1), var(--color2), var(--color1));
+  background-size: 200% 80%;
+  background-position-y: 50%;
+  background-repeat: repeat-x;
+  animation: ${load} 1.5s ease infinite;
+`
+
+function Media({
+  image,
+  order = 0,
+  isLoading = false,
+}: MediaProps & { order?: number | number[]; isLoading?: boolean }) {
   return (
     <Box
-      as="img"
-      src={image}
+      as={isLoading ? MediaPlaceholder : 'img'}
+      src={isLoading ? undefined : image}
       alt=""
       order={order}
       aspectRatio="1.88 / 1" // 1024:545 medium article thumbnail ratio
