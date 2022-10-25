264246651293d9835f09a14dec6997007730f3e9
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2022-10-25 12:46:19 +0200

    About page: hero video (#553)

--- src/components/HeroVideo.tsx
+++ src/components/HeroVideo.tsx
@@ -2,19 +2,28 @@ import { Box, Stack, Text } from '@centrifuge/fabric'
 import { graphql } from 'gatsby'
 import * as React from 'react'
 import { CenterContainer } from './CenterContainer'
+import { YoutubeEmbed } from './YoutubeEmbed'
 
 export const query = graphql`
   fragment HeroVideoFragment on DataJsonHero_video {
     title
     body
-    video
+    video {
+      youtubeId
+    }
   }
 `
 
 export type HeroVideoProps = {
   title: string
   body: string
-  video: string
+  video:
+    | {
+        youtubeId: string
+      }
+    | {
+        url: string
+      }
 }
 
 export function HeroVideo({ title, body, video }: HeroVideoProps) {
@@ -29,18 +38,13 @@ export function HeroVideo({ title, body, video }: HeroVideoProps) {
             {body}
           </Text>
         </Box>
-        {/* @ts-expect-error `width` prop type conflicts with video element `width` attribute */}
-        <Box
-          as="video"
-          aspectRatio="16 / 9"
-          alignSelf="flex-end"
-          width={['100%', '100%', '75%']}
-          src={video}
-          autoPlay
-          muted
-          loop
-          mt={6}
-        />
+        <Box alignSelf="flex-end" width={['100%', '100%', '75%']} mt={6}>
+          {'youtubeId' in video ? (
+            <YoutubeEmbed videoId={video.youtubeId} width="100%" />
+          ) : (
+            <Box as="video" aspectRatio="16 / 9" width="100%" src={video.url} autoPlay muted loop />
+          )}
+        </Box>
       </Stack>
     </CenterContainer>
   )
