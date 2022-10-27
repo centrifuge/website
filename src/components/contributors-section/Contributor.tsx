c77d00512d013fab90669eed5f7ab5062174b62f
Hornebom <you@hornebom.com>
2022-10-27 18:51:10 +0200

    refactors ContributorsSection
    - refactors to use embla carousel

---
+++ src/components/contributors-section/Contributor.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Grid, Box, Stack, Text } from '@centrifuge/fabric'
+import { Image, ImageProps } from '../Image'
+import link from '../../assets/link.svg'
+
+export type ContributorProps = {
+  name: string
+  role: string
+  social: string
+  image: ImageProps
+}
+
+export function Contributor({ item }: { item: ContributorProps }) {
+  return (
+    <Grid
+      as="li"
+      gridTemplateColumns={['minmax(0, 38vw) 1fr', '1fr']}
+      width={['100%', 200, 260]}
+      flexShrink={0}
+      gap={['5vw', 2]}
+      px={[0, 2]}
+      alignItems="center"
+    >
+      <Box position="relative" pl={[20, 0]}>
+        <Box
+          borderRadius="50%"
+          overflow="hidden"
+          style={{
+            // New stacking context to fix border radius in Safari
+            isolation: 'isolate',
+          }}
+        >
+          <Image data={item.image} />
+        </Box>
+        {item.social && (
+          <Box
+            as="a"
+            href={item.social}
+            rel="noopener noreferrer"
+            target="_blank"
+            title={`Find ${item.name} on Social media`}
+            position="absolute"
+            top={[0, 'initial']}
+            left={[0, 'initial']}
+            bottom={['initial', 0]}
+            right={['initial', 0]}
+          >
+            <Box as="img" display="block" src={link} maxWidth={[24, 30]} alt="" />
+          </Box>
+        )}
+      </Box>
+      <Stack alignItems={['flex-start', 'center']} textAlign={['left', 'center']} flexShrink={0}>
+        <Text variant="body1" as="h3">
+          {item.name}
+        </Text>
+        <Text variant="body2" color="textSecondary" as="span">
+          {item.role}
+        </Text>
+      </Stack>
+    </Grid>
+  )
+}
---
+++ src/components/contributors-section/MobileCarousel.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import { Shelf, Box, Stack, IconArrowLeft, IconArrowRight } from '@centrifuge/fabric'
+import { Contributor, ContributorProps } from './Contributor'
+import { Viewport, Control } from './styles'
+
+type MobileCarouselProps = {
+  items: ContributorProps[]
+}
+
+const ITEMS_PER_CHUNK = 5
+
+export function MobileCarousel({ items }: MobileCarouselProps) {
+  const chunks: ContributorProps[][] = items.reduce((all, one, index) => {
+    const chunkIndex = Math.floor(index / ITEMS_PER_CHUNK)
+    all[chunkIndex] = [].concat(all[chunkIndex] || [], one)
+    return all
+  }, [])
+
+  const [viewportRef, embla] = useEmblaCarousel({
+    skipSnaps: false,
+  })
+
+  const [prevBtnEnabled, setPrevBtnEnabled] = React.useState(false)
+  const [nextBtnEnabled, setNextBtnEnabled] = React.useState(false)
+
+  const scrollPrev = React.useCallback(() => embla && embla.scrollPrev(), [embla])
+  const scrollNext = React.useCallback(() => embla && embla.scrollNext(), [embla])
+
+  const onSelect = React.useCallback(() => {
+    if (!embla) {
+      return
+    }
+
+    setPrevBtnEnabled(embla.canScrollPrev())
+    setNextBtnEnabled(embla.canScrollNext())
+  }, [embla])
+
+  React.useEffect(() => {
+    if (!embla) {
+      return
+    }
+
+    onSelect()
+    embla.on('select', onSelect)
+
+    return () => embla && embla.destroy()
+  }, [embla, onSelect])
+
+  return (
+    <Box display={['block', 'none']}>
+      <Shelf justifyContent="end" px={2} gap={1}>
+        <Control type="button" title="Previous" onClick={scrollPrev} disabled={!prevBtnEnabled}>
+          <IconArrowLeft />
+        </Control>
+        <Control type="button" title="Next" onClick={scrollNext} disabled={!nextBtnEnabled}>
+          <IconArrowRight />
+        </Control>
+      </Shelf>
+
+      <Viewport ref={viewportRef} overflow="hidden" mt={2}>
+        <Shelf>
+          {chunks.map((chunk) => (
+            <Stack key={chunk[0].name} as="ul" gap={4} flexShrink={0} flexBasis="100%" flexGrow={0} px={2}>
+              {chunk.map((item) => (
+                <Contributor item={item} key={item.name} />
+              ))}
+            </Stack>
+          ))}
+        </Shelf>
+      </Viewport>
+    </Box>
+  )
+}
