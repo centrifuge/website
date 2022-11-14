503a56a6f33671c218a768176d0ba0bf897fa1b2
Hornebom <you@hornebom.com>
2022-11-14 08:44:49 +0100

    set Response options + prototype NewsSection

--- lambda/index.ts
+++ lambda/index.ts
@@ -2,12 +2,17 @@ import routes from './routes.json'
 import { Request, Response } from 'express'
 
 exports.handler = async (req: Request, res: Response) => {
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8000')
+  res.set('Access-Control-Allow-Methods', 'GET')
+
   if (routes.length < 0) {
     return res.status(400).send('No functions defined')
   }
+
   for (let route of routes) {
     if (req.path.replace('/', '') === route.name) {
       const method = require(`./src/${route.name}`)
+
       return method.default(req, res, route?.options ?? {})
     }
   }
