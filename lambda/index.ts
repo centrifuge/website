ceb0a68816b3f62efeb540b1ee71f03c1a7123d9
Hornebom <you@hornebom.com>
2022-11-15 16:37:33 +0100

    refactor lambdas

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,12 +1,21 @@
 import routes from './routes.json'
 import { Request, Response } from 'express'
 
-exports.handler = async (req: Request, res: Response) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:8000')
-  res.set('Access-Control-Allow-Methods', 'GET')
+const corsWhitelist = ['http://localhost:8000', 'https://centrifuge.io']
+const testEnvRegex = new RegExp('https://[a-z0-9-]*test-website-centrifuge-2022.netlify.app')
 
+exports.handler = async (req: Request, res: Response) => {
   if (routes.length < 0) {
     return res.status(400).send('No functions defined')
+  }
+
+  const origin = req.get('origin')
+
+  if (corsWhitelist.indexOf(origin) !== -1 || testEnvRegex.test(origin)) {
+    res.set('Access-Control-Allow-Origin', origin)
+    res.set('Access-Control-Allow-Methods', 'GET')
+  } else {
+    return res.status(405).send('Not allowed')
   }
 
   for (let route of routes) {
